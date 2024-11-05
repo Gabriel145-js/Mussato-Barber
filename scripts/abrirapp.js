87636f6b637e5b0cb4df085f93ff2c6adee0169d
Gabriel145-js <gabrielzapa456@gmail.com>
2024-11-05 18:20:44 -0300

    test: js do abrirapp

--- scripts/abrirapp.js
+++ scripts/abrirapp.js
@@ -1,8 +1,8 @@
 function abrirApp (){
 
     //Links app
-    var androidLink = 'https://play.google.com/store/search?q=app+barber&c=apps'
-    var iosLink = 'https://play.google.com/store/search?q=app+barber&c=apps'
+    var androidLink = 'vnd.appbarber://'
+    var iosLink = 'https://chatgpt.com/'
 
     //Links para as lojas do app (Android ou IOS)
     var androidLinkLoja = 'https://play.google.com/store/search?q=app+barber&c=apps'
