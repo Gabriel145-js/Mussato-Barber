726a037ca3960a0477eb8783c452c86c7acbec00
Gabriel145-js <gabrielzapa456@gmail.com>
2024-11-05 21:49:18 -0300

    test

--- scripts/abrirapp.js
+++ scripts/abrirapp.js
@@ -1,7 +1,8 @@
 function abrirApp() {
     // Links para abrir o Instagram diretamente
     var androidLink = 'intent://instagram.com/#Intent;package=com.instagram.android;scheme=https;end';
-    var iosLink = 'appbarber-cliente://';
+    var iosLink = 'https://appbarber.com.br';  // O link que abre o app, ou sua página na web, caso o app não esteja instalado
+
 
     // Links para as lojas de aplicativos (Android ou iOS)
     var androidLinkLoja = 'https://play.google.com/store/apps/details?id=com.instagram.android';
