160f965200caa66a1fffe5209d91ac40d6c70c32
Gabriel145-js <gabrielzapa456@gmail.com>
2025-01-28 23:19:34 -0300

    styles:responsividade styles: adição de novas imagens

--- scripts/carrosel.js
+++ scripts/carrosel.js
@@ -5,27 +5,22 @@ const antes = document.getElementById('antes')
 
 let contador = 0
 
-// Inicializa a primeira foto como visível
 fotos[0].classList.add('visible')
 
 function aparecerFoto() {
-  // Remove a classe 'visible' de todas as fotos
   fotos.forEach(foto => {
     foto.classList.remove('visible')
   })
 
-  // Adiciona a classe 'visible' à foto atual
   fotos[contador].classList.add('visible')
 
-  // Move o carrossel para a esquerda
   carrossel.style.transform = `translateX(-${contador * 100}%)`
 
-  // Incrementa o contador e ajusta para loop contínuo
   contador = (contador + 1) % fotos.length
 }
 
-// Chama a função aparecerFoto a cada 3 segundos
-setInterval(aparecerFoto, 3000)
+
+setInterval(aparecerFoto, 4000)
 
 // Responsável por abrir a janela modal do botão "Pagar agora"
 function abrilModal() {
